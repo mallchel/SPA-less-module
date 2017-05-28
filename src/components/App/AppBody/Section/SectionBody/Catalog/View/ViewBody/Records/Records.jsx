00bac496abb064ad525d1c4cb18b1dd9de30a932
mallchel <sebastianalfabc@gmail.com>
2017-05-28 17:33:23 +0300

    add type to table

--- src/components/App/AppBody/Section/SectionBody/Catalog/View/ViewBody/Records/Records.jsx
+++ src/components/App/AppBody/Section/SectionBody/Catalog/View/ViewBody/Records/Records.jsx
@@ -1,38 +1,14 @@
 import React from 'react'
 import _ from 'lodash'
 import Reflux from 'reflux'
-import ReactDataGrid from 'react-data-grid'
 import UserSettingsStore from '../../../../../../../../../stores/UserSettingsStore'
 import recordActions from '../../../../../../../../../actions/recordActions'
+import RecordsData from './RecordsData'
 
 const Records = React.createClass({
   mixins: [Reflux.listenTo(UserSettingsStore, "onUserSettings")],
   propTypes: {
     catalog: React.PropTypes.object,
-  },
-
-  getInitialState() {
-    this.createRows();
-    return null;
-  },
-
-  createRows() {
-    let rows = [];
-    for (let i = 1; i < 10; i++) {
-      rows.push({
-        id: i,
-        title: 'Title ' + i,
-        count: i
-      });
-    }
-
-    this._rows = rows;
-  },
-
-  rowGetter(i) {
-    console.log(this._records[i], i)
-    // return this._rows[i];
-    return this._records[i];
   },
 
   // refactor
@@ -63,30 +39,23 @@ const Records = React.createClass({
 
   render() {
     const catalog = this.props.catalog;
-    this._fieldsToRender = [];
+    this.fieldsToRender = [];
+
     if (catalog) {
-      const fields = catalog.get('fields');
+      this.fields = catalog.get('fields');
+      this.records = catalog.get('records');
+
       const catalogId = catalog.get('id');
-      this._columns = fields && fields.toJS().map(field => {
-        return {
-          key: field.id,
-          name: field.name
-        }
-      });
-      this._records = catalog.get('records') && catalog.get('records').toJS().map(record => {
-        return record.values;
-      })
+
       // get sort info from user settings.
       let sortingRecordSetting = UserSettingsStore.getSortingRecords({ catalogId });
     }
     return (
-      // console.log(this._columns, this._rows),
-      this._columns && this._records ?
-        <ReactDataGrid
-          columns={this._columns}
-          rowGetter={this.rowGetter}
-          rowsCount={this._records.length}
-          minHeight={500}
+      this.fields && this.records
+        ?
+        <RecordsData
+          fields={this.fields}
+          records={this.records}
         />
         :
         null
